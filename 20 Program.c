c9386bcf81d4b96ae3658b7dc1d5e74c8a1a9f23
Pratham Aggarwal <126560546+iprattham@users.noreply.github.com>
2026-04-16 17:36:06 +0530

    Bouncing Ball Animation 🏀↑☄️

---
+++ 20 Program.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<conio.h>
+#include<graphics.h>
+#include<dos.h>
+void main()
+{
+ int gd=DETECT,gm=DETECT;
+ int x,y=0,j,t=400,c=1;
+ initgraph(&gd,&gm,"");
+ setcolor(RED);
+ setfillstyle(SOLID_FILL,RED);
+ for(x=40;x<602;x++)
+ {
+  cleardevice();
+  circle(x,y,30);
+  floodfill(x,y,RED);
+  delay(10);
+  if(y>=400)
+  {
+   c=0;
+   t-=20;
+  }
+  if(y<=(400-t))
+  c=1;
+  y=y+(c?15:-15);
+ }
+ printf("\n\n\n\nPratham - 00113702022");
+ getch();
+}
